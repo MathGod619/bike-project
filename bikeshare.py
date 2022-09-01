3316789b4299288f9e67b7204c2a420c07cacffc
Anthony Putaro <mr.putar@gmail.com>
2022-08-31 20:21:56 -0700

    added my name to the project credits

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+##CREDIT TO ANTHONY PUTARO##
     # data to be used in the program
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
